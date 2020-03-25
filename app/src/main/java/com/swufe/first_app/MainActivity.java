ddd152da962d709b76833bb5afd60b5b07ce63f2
Daicius <210413549@qq.com>
2020-03-25 17:12:27 +0800

    change1

--- app/src/main/java/com/swufe/first_app/MainActivity.java
+++ app/src/main/java/com/swufe/first_app/MainActivity.java
@@ -9,23 +9,36 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 TextView score;
+    TextView score2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         score=(TextView) findViewById(R.id.score);
+        score2=(TextView) findViewById(R.id.score2);
+
     }
     public  void btnAdd1(View btn){
-        showScore(1);
+        if(btn.getId()==R.id.btn_1){ showScore(1);
+            }
+        else{ showScore2(1);
+        }
     }
     public  void btnAdd2(View btn){
-        showScore(2);
+        if(btn.getId()==R.id.btn_2){ showScore(2);
+        }
+        else{ showScore2(2);
+        }
     }
     public  void btnAdd3(View btn){
-        showScore(3);
+        if(btn.getId()==R.id.btn_3){ showScore(3);
+        }
+        else{ showScore2(3);
+        }
     }
     public  void btnReset(View btn){
         score.setText("0");
+        score2.setText("0");
     }
     private void showScore(int inc){
         Log.i("show","inc"+inc);
@@ -33,6 +46,11 @@ TextView score;
         int newScore  = Integer.parseInt(oldscore)+inc;
         score.setText(""+newScore);
     }
-
+    private void showScore2(int inc){
+        Log.i("show","inc"+inc);
+        String oldscore = (String) score2.getText();
+        int newScore  = Integer.parseInt(oldscore)+inc;
+        score2.setText(""+newScore);
+    }
 
 }
